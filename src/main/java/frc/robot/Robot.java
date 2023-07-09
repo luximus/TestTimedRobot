3eeb484e2e667b0f31802b5ff61934342dbe6a83
Ivan Post <ivan.post24@gmail.com>
2023-07-09 12:47:14 -0600

    Add intake and shooter motor control

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,7 @@ import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj.motorcontrol.Talon;
 
 /**
@@ -22,62 +23,111 @@ public class Robot extends TimedRobot {
    * initialization code.
    */
 
-   Talon leftDriveMotorController = new Talon(RobotMap.LEFT_DRIVE_MOTOR_CONTROLLER_CHANNEL_ID);
-   Talon rightDriveMotorController = new Talon(RobotMap.RIGHT_DRIVE_MOTOR_CONTROLLER_CHANNEL_ID);
-   DifferentialDrive driveTrain = new DifferentialDrive(leftDriveMotorController, rightDriveMotorController);
-   
-   XboxController driverController = new XboxController(RobotMap.DRIVER_CONTROLLER_PORT);
+    Talon leftDriveMotorController = new Talon(RobotMap.LEFT_DRIVE_MOTOR_CONTROLLER_CHANNEL_ID);
+    Talon rightDriveMotorController = new Talon(RobotMap.RIGHT_DRIVE_MOTOR_CONTROLLER_CHANNEL_ID);
+    DifferentialDrive driveTrain = new DifferentialDrive(leftDriveMotorController, rightDriveMotorController);
 
-   SlewRateLimiter driveAccelerationLimiter = new SlewRateLimiter(RobotMap.RAMP_RATE);
+    XboxController driverController = new XboxController(RobotMap.DRIVER_CONTROLLER_PORT);
+    SlewRateLimiter driveAccelerationLimiter = new SlewRateLimiter(RobotMap.RAMP_RATE); 
+    Talon leftIntakeMotorController = new Talon(RobotMap.LEFT_INTAKE_MOTOR_CONTROLLER_CHANNEL_ID);
+    Talon rightIntakeMotorController = new Talon(RobotMap.RIGHT_INTAKE_MOTOR_CONTROLLER_CHANNEL_ID);
+    MotorControllerGroup intakeMotorControllerGroup = new MotorControllerGroup(leftDriveMotorController, rightDriveMotorController);    
+    Talon indexerMotorController = new Talon(RobotMap.INDEXER_MOTOR_CONTROLLER_CHANNEL_ID);
+    Talon shooterFeedMotorController = new Talon(RobotMap.SHOOTER_FEED_MOTOR_CONTROLLER_CHANNEL_ID);
+    Talon shooterMotorController = new Talon(RobotMap.SHOOTER_MOTOR_CONTROLLER_CHANNEL_ID); 
+    XboxController operatorController = new XboxController(RobotMap.OPERATOR_CONTROLLER_PORT);
+    SlewRateLimiter intakeRateLimiter = new SlewRateLimiter(RobotMap.RAMP_RATE);
+    SlewRateLimiter indexerRateLimiter = new SlewRateLimiter(RobotMap.RAMP_RATE);
+    SlewRateLimiter shooterFeedRateLimiter = new SlewRateLimiter(RobotMap.RAMP_RATE);
+    SlewRateLimiter shooterRateLimiter = new SlewRateLimiter(RobotMap.RAMP_RATE);
 
-  @Override
-  public void robotInit() {}
-
-  @Override
-  public void robotPeriodic() {}
-
-  @Override
-  public void autonomousInit() {}
-
-  @Override
-  public void autonomousPeriodic() {}
-
-  @Override
-  public void teleopInit() {
-  }
-
-  @Override
-  public void teleopPeriodic() {
-    double speedMultiplier;
-    if (driverController.getRawButtonPressed(RobotMap.DRIVER_SLOW_SPEED_BUTTON.value)) {
-      speedMultiplier = RobotMap.SLOW_SPEED_MULTIPLIER;
-    } else if (driverController.getRawButton(RobotMap.DRIVER_FAST_SPEED_BUTTON.value)) {
-      speedMultiplier = RobotMap.FAST_SPEED_MULTIPLIER;
-    } else {
-      speedMultiplier = RobotMap.DEFAULT_SPEED_MULTIPLIER;
+    @Override
+    public void robotInit() {
+        intakeMotorControllerGroup.setInverted(RobotMap.INTAKE_IS_INVERTED);
+        indexerMotorController.setInverted(RobotMap.INDEXER_IS_INVERTED);
+        shooterFeedMotorController.setInverted(RobotMap.SHOOTER_FEED_IS_INVERTED);
+        shooterMotorController.setInverted(RobotMap.SHOOTER_IS_INVERTED);
     }
-    double leftInput = driveAccelerationLimiter.calculate(
-      speedMultiplier*driverController.getRawAxis(RobotMap.DRIVER_LEFT_CONTROL_AXIS.value));
-    double rightInput = driveAccelerationLimiter.calculate(
-      speedMultiplier*driverController.getRawAxis(RobotMap.DRIVER_RIGHT_CONTROL_AXIS.value));
-    driveTrain.tankDrive(leftInput, rightInput);
-  }
 
-  @Override
-  public void disabledInit() {}
+    @Override
+    public void robotPeriodic() {}
 
-  @Override
-  public void disabledPeriodic() {}
+    @Override
+    public void autonomousInit() {}
 
-  @Override
-  public void testInit() {}
+    @Override
+    public void autonomousPeriodic() {}
 
-  @Override
-  public void testPeriodic() {}
+    @Override
+    public void teleopInit() {
+    }
 
-  @Override
-  public void simulationInit() {}
+    @Override
+    public void teleopPeriodic() {
+        // Drive
+        double speedMultiplier;
+        if (driverController.getRawButtonPressed(RobotMap.DRIVER_SLOW_SPEED_BUTTON.value)) {
+            speedMultiplier = RobotMap.SLOW_SPEED_MULTIPLIER;
+        } else if (driverController.getRawButton(RobotMap.DRIVER_FAST_SPEED_BUTTON.value)) {
+            speedMultiplier = RobotMap.FAST_SPEED_MULTIPLIER;
+        } else {
+            speedMultiplier = RobotMap.DEFAULT_SPEED_MULTIPLIER;
+        }
+        double leftInput = driveAccelerationLimiter.calculate(
+            speedMultiplier*driverController.getRawAxis(RobotMap.DRIVER_LEFT_CONTROL_AXIS.value));
+        double rightInput = driveAccelerationLimiter.calculate(
+            speedMultiplier*driverController.getRawAxis(RobotMap.DRIVER_RIGHT_CONTROL_AXIS.value));
+        driveTrain.tankDrive(leftInput, rightInput);
 
-  @Override
-  public void simulationPeriodic() {}
+        // Intake and shooter
+        double intakeSpeed;
+        double indexerSpeed;
+        double shooterFeedSpeed;
+        double shooterSpeed;
+        if (operatorController.getRawButton(RobotMap.OPERATOR_ALL_REVERSE_BUTTON.value)) {
+            intakeSpeed = -RobotMap.INTAKE_REVERSE_SPEED;
+            indexerSpeed = -RobotMap.INDEXER_REVERSE_SPEED;
+            shooterFeedSpeed = -RobotMap.SHOOTER_FEED_REVERSE_SPEED;
+            shooterSpeed = -RobotMap.SHOOTER_REVERSE_SPEED;
+        } else {
+            if (operatorController.getRawButton(RobotMap.OPERATOR_INTAKE_AND_INDEXER_CONTROL_BUTTON.value)) {
+                intakeSpeed = RobotMap.INTAKE_SPEED;
+                indexerSpeed = RobotMap.INDEXER_SPEED;
+            } else {
+                intakeSpeed = 0.0d;
+                indexerSpeed = 0.0d;
+            }
+
+            if (operatorController.getRawButton(RobotMap.OPERATOR_SHOOTER_CONTROL_BUTTON.value)) {
+                shooterFeedSpeed = RobotMap.SHOOTER_FEED_SPEED;
+                shooterSpeed = RobotMap.SHOOTER_SPEED;
+            } else {
+                shooterFeedSpeed = 0.0d;
+                shooterSpeed = 0.0d;
+            }
+        }
+
+        intakeMotorControllerGroup.set(intakeRateLimiter.calculate(intakeSpeed));
+        indexerMotorController.set(indexerRateLimiter.calculate(indexerSpeed));
+        shooterFeedMotorController.set(shooterFeedRateLimiter.calculate(shooterFeedSpeed));
+        shooterMotorController.set(shooterRateLimiter.calculate(shooterSpeed));
+    }
+
+    @Override
+    public void disabledInit() {}
+
+    @Override
+    public void disabledPeriodic() {}
+
+    @Override
+    public void testInit() {}
+
+    @Override
+    public void testPeriodic() {}
+
+    @Override
+    public void simulationInit() {}
+
+    @Override
+    public void simulationPeriodic() {}
 }
--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -6,15 +6,26 @@ public final class RobotMap {
     // Motor controller channel IDs
     public static final int LEFT_DRIVE_MOTOR_CONTROLLER_CHANNEL_ID = 0;
     public static final int RIGHT_DRIVE_MOTOR_CONTROLLER_CHANNEL_ID = 1;
+    public static final int LEFT_INTAKE_MOTOR_CONTROLLER_CHANNEL_ID = 2;
+    public static final int RIGHT_INTAKE_MOTOR_CONTROLLER_CHANNEL_ID = 3;
+    public static final int INDEXER_MOTOR_CONTROLLER_CHANNEL_ID = 4;
+    public static final int SHOOTER_FEED_MOTOR_CONTROLLER_CHANNEL_ID = 5;
+    public static final int SHOOTER_MOTOR_CONTROLLER_CHANNEL_ID = 6;
 
     // Input controller port numbers
     public static final int DRIVER_CONTROLLER_PORT = 0;
+    public static final int OPERATOR_CONTROLLER_PORT = 1;
 
     // Buttons and axes for driver control
     public static final XboxController.Axis DRIVER_LEFT_CONTROL_AXIS = XboxController.Axis.kLeftTrigger;
     public static final XboxController.Axis DRIVER_RIGHT_CONTROL_AXIS = XboxController.Axis.kRightTrigger;
     public static final XboxController.Button DRIVER_SLOW_SPEED_BUTTON = XboxController.Button.kLeftBumper;
     public static final XboxController.Button DRIVER_FAST_SPEED_BUTTON = XboxController.Button.kRightBumper;
+
+    // Buttons and axes for operator control
+    public static final XboxController.Button OPERATOR_INTAKE_AND_INDEXER_CONTROL_BUTTON = XboxController.Button.kLeftBumper;
+    public static final XboxController.Button OPERATOR_SHOOTER_CONTROL_BUTTON = XboxController.Button.kRightBumper;
+    public static final XboxController.Button OPERATOR_ALL_REVERSE_BUTTON = XboxController.Button.kB;
 
     // Speed button multipliers
     public static final double SLOW_SPEED_MULTIPLIER = 0.2;
@@ -23,4 +34,19 @@ public final class RobotMap {
 
     // Ramping
     public static final double RAMP_RATE = 0.1;
+
+    // Static motor speed multipliers
+    public static final double INTAKE_SPEED = 0.8;
+    public static final double INTAKE_REVERSE_SPEED = 0.2;
+    public static final boolean INTAKE_IS_INVERTED = false;
+    public static final double INDEXER_SPEED = 0.5;
+    public static final double INDEXER_REVERSE_SPEED = 0.2;
+    public static final boolean INDEXER_IS_INVERTED = false;
+    public static final double SHOOTER_FEED_SPEED = 0.8;
+    public static final double SHOOTER_FEED_REVERSE_SPEED = 0.2;
+    public static final boolean SHOOTER_FEED_IS_INVERTED = false;
+    public static final double SHOOTER_SPEED = 0.8;
+    public static final double SHOOTER_REVERSE_SPEED = 0.2;
+    public static final boolean SHOOTER_IS_INVERTED = false;
+
 }
